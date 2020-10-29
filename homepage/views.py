b763b387014e1d2c97e314b928ebf75f5b4148ed
Aleksandr Tolmachev <a.tolmachev@xsolla.com>
2020-10-29 11:29:12 +0500

    add tank data

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 import deeppavlov
 
+#test
+path = './data/tanks/British Tanks/FV4202 105.txt'
+with open(path, 'r') as file:
+    context = file.read().replace('\n', '. ')
 
 def index(request):
     question = ''
